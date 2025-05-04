dfebf5452f8c808fd2705187a33a6eb500ad11a1
Atuh Fon <115895139+hutaa@users.noreply.github.com>
2025-05-04 16:37:23 -0400

    Implemented registration buttons and info symbols

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,9 +1,42 @@
-import Q3Logo from './Q3LOGO_notext.png'
+import infoSym from './regInfoSymbol.png'
+
 function Registration() {
   return (
     <div>
-      <h1>Registration</h1>
-      <img src={Q3Logo} class = "logo-img" alt="Q3 Logo no text"></img>
+      <div className="blob-effect">
+        <h1>Registration</h1>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="reg-btn">
+        <div className="btn-col">
+          <button id="vol-btn">Volunteer</button>
+          <button className="vol-sym-btn">
+            <img src={infoSym} alt="Volunteer info symbol icon" />
+            <div className="popup-box">
+              As a volunteer, you play a key role in ensuring the success of our initiative.
+              From managing logistics to supporting participants, your efforts help foster a smooth and welcoming experience for everyone involved!
+            </div>
+          </button>
+          
+          
+        </div>
+
+        <div className="btn-col">
+          <button id="explore-btn">Explorer</button>
+          <button className="exp-sym-btn">
+            <img src={infoSym} alt="Explorer info symbol icon" />
+            <div className="popup-box">
+              As an explorer, you will explore the intricacies of Quantum in Art and Ethics.
+            </div>
+          
+          </button>
+          
+          
+          
+        </div>
+      </div>
     </div>
   );
 }
