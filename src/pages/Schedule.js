be317165b6c026b99265a5a7fc07738b018b499c
Sophia Jin <sjin6948@terpmail.umd.edu>
2025-05-24 22:33:06 -0400

    schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -1,8 +1,106 @@
+import '../styles/Schedule.css';
 function Schedule() {
   return (
     <div>
       <h1>Schedule</h1>
-      <p>This is the schedule page.</p>
+     {/*Friday */}
+      <h3 style={{fontFamily: 'Poppins', marginLeft: '200px'}}>Friday,</h3>
+      <h4 style={{fontFamily: 'Poppins',marginLeft: '199px', marginTop: '-5px',marginBottom: '10px'}}>September 12</h4>
+      <div class="flex-container" style={{marginLeft: '50px'}}>
+       <div class="flex-item white" style={{marginTop: '50px'}}>
+       <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Welcome Dinner Reception</h5>
+       <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter', fontWeight: 'normal'}}>and Registration</h5>
+       <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Exhibition: Quantum Choreobotics</h5>
+       <h5 style={{marginTop: '-60px', marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>4 - 6 PM</h5>
+       <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+       <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+      </div>
+      </div>
+    {/*Saturday */}
+    <h3 style={{fontFamily: 'Poppins', marginLeft: '650px', marginTop: '-1367px'}}>Saturday,</h3>
+      <h4 style={{fontFamily: 'Poppins',marginLeft: '650px', marginTop: '-5px',marginBottom: '10px'}}>September 13</h4>
+      <div class="flex-container" style={{marginLeft: '510px'}}>
+         <div class="flex-item white" style={{marginTop: '50px'}}>
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Breakfast</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>9 - 10 AM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white" >
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Opening Remarks</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Quantum and Art</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>- Victoria Kumaran</h5>
+          <h5 style={{ marginTop: '-60px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>9:30 - 10 AM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white" >
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Keynote 2</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Prof. Nicole Yunger Halpern</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>& Burce Modvic</h5>
+          <h5 style={{ marginTop: '-60px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>10:10 - 11 AM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white" >
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Workshop 1</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Quantum Comics </h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Using Illustrator</h5>
+          <h5 style={{ marginTop: '-60px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>11:10 - 12:10 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white">
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Lunch</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>12:10 - 1 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white">
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Keynote 3</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>1:10 - 2:10 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white" >
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Workshop 2</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Quantum Chip Rendering </h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Using Blender</h5>
+          <h5 style={{ marginTop: '-60px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>2:20 - 3:20 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white">
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Networking Break</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>3:20 - 4 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white" >
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Workshop 3</h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Animations Using </h5>
+          <h5 style={{marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Manim / After Effects</h5>
+          <h5 style={{ marginTop: '-60px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>4:10 - 5:10 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white">
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Quantum Intuition Demo</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>5:10 - 5:45 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+         <div class="flex-item white">
+          <h5 style={{marginTop: '20px',marginLeft: '15px' ,  fontFamily: 'Inter' , fontWeight: 'normal'}}>Closing Remarks</h5>
+          <h5 style={{marginTop: '-21px',marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'bold'}}>5:50 - 6:15 PM</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Stamp Student</h5>
+          <h5 style={{ marginLeft: '280px' ,  fontFamily: 'Inter', fontWeight: 'lighter'}}>Union</h5>
+         </div>
+      </div>
+    {/*Sunday*/}
+    <h3 style={{fontFamily: 'Poppins', marginLeft: '1125px', marginTop: '-1367px'}}>Sunday,</h3>
+    <h4 style={{fontFamily: 'Poppins',marginLeft: '1125px', marginTop: '-5px',marginBottom: '10px'}}>September 14</h4>
+    <div class="flex-container" style={{marginLeft: '970px', marginBottom: '20px'}}></div>
     </div>
   );
 }
