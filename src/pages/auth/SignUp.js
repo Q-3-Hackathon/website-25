dd5f32c175cce5ef12bbed238a06499117d3c3f3
Pranavsai Akurati <psakurati@gmail.com>
2025-06-03 23:36:26 -0400

    added more questions for explorers, made important user fields mandatory to fill out

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -12,14 +12,11 @@ function SignUp() {
     const lastName = useRef('');
     const email = useRef('');
     const phoneNum = useRef('');
-    const streetAddr = useRef('');
-    const streetAddrTwo = useRef('');
-    const city = useRef('');
-    const state = useRef('');
-    const zipCode = useRef('');
     const institution = useRef('');
     const role = useRef('');
+    const otherRole = useRef('');
     const whereHeard = useRef('');
+    const otherWhereHeard = useRef('');
     const emergFirstName = useRef('');
     const emergLastName = useRef('');
     const emergRelation = useRef('');
@@ -34,12 +31,15 @@ function SignUp() {
     // explorer
     const major = useRef('');
     const attendance = useRef('');
-    const why = useRef('');
+    const whyQuantumArt = useRef('');
     const favoriteSong = useRef('');
     const favoriteMovie = useRef('');
-    const favoriteMuseum = useRef('');
+    const favoriteBook = useRef('')
+    const favoriteGame = useRef('');
+    const bookPrompt = useRef('');
     const artPrompt = useRef('');
-    const ethicsPrompt = useRef('');
+    const researchPrompt = useRef('');
+    const q3Prompt = useRef('');
 
     {/* Need to implement terms and conditions for the forms */}
 
@@ -56,18 +56,28 @@ function SignUp() {
 
     // multiple choice questions
     const [selectedLocations, setSelectedLocations] = useState([]);
+    const [locationError, setLocationError] = useState('');
 
     const handleCheckboxChange = (e) => {
         const { value, checked } = e.target;
         setSelectedLocations((prev) =>
             checked ? [...prev, value] : prev.filter((v) => v !== value)
         );
+
+        if (locationError) { 
+            setLocationError('');
+        }
     };
 
     const [submitted, setSubmitted] = React.useState(false);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (selectedLocations.length === 0) {
+            setLocationError('Please select at least one option')
+            return;
+        }
 
         if (!canSubmit.current)
             return;
@@ -78,14 +88,11 @@ function SignUp() {
             lastName: lastName.current.value,
             email: email.current.value,
             phoneNum: phoneNum.current.value,
-            streetAddr: streetAddr.current.value,
-            streetAddrTwo: streetAddrTwo.current.value,
-            city: city.current.value,
-            state: state.current.value,
-            zipCode: zipCode.current.value,
             institution: institution.current.value,
             role: role.current.value,
-            whereHeard: whereHeard.current.value
+            otherRole: otherRole.current.value,
+            whereHeard: whereHeard.current.value,
+            otherWhereHeard: whereHeard.current.value
         };
 
         if (userType === 'volunteer') {
@@ -95,12 +102,15 @@ function SignUp() {
         } else if (userType === 'explorer') {
             data.major = major.current.value;
             data.attendance = attendance.current.value;
-            data.why = why.current.value;
+            data.whyQuantumArt = whyQuantumArt.current.value;
             data.favoriteSong = favoriteSong.current.value;
             data.favoriteMovie = favoriteMovie.current.value;
-            data.favoriteMuseum = favoriteMuseum.current.value;
+            data.favoriteGame = favoriteGame.current.value;
+            data.favoriteBook = favoriteBook.current.value;
             data.artPrompt = artPrompt.current.value;
-            data.ethicsPrompt = ethicsPrompt.current.value;
+            data.bookPrompt = bookPrompt.current.value;
+            data.researchPrompt = researchPrompt.current.value;
+            data.q3Prompt = q3Prompt.current.value;
         }
 
         // Add a new document with a generated ID
@@ -134,40 +144,18 @@ function SignUp() {
 
                 <br />
 
-                <div className="form-group">
-                <label htmlFor="full-name" classname="form-label">Name:</label>
-                    <div id="full-name" className={styles.formRow}>
-                        <TextInput label="First Name" ref={firstName} />
-                        <TextInput label="Last Name" ref={lastName} />
+                <div className={styles.formGroup}>
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <div className={styles.formRow}>
+                        <TextInput id="firstName" label="First Name" ref={firstName} required={true} style={{ flex: 1 }} />
+                        <TextInput id="lastName" label="Last Name" ref={lastName} required={true} style={{ flex: 1 }} />
                     </div>
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="street-address" classname="form-label">Address:</label>
-                    <TextInput id="street-address" label="Street Address" ref={streetAddr} />
-                </div>
-                <TextInput label="Street Address Line 2" ref={streetAddrTwo} />
-                <TextInput label="City" ref={city} />
+                <TextInput label="Email" ref={email} showLabel={true} type="email" placeholder="ex: myname@example.com" required={true} />
+                <TextInput label="Phone Number" ref={phoneNum} showLabel={true} placeholder="(000) 000-000" required={true} />
 
-                <div className={styles.formRow}>
-                    <TextInput label="State" ref={state} />
-                    <TextInput label="Zip Code" ref={zipCode} />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="email" classname="form-label">Email:</label>
-                    <TextInput id="email" label="ex: myname@example.com" ref={email} type="email" />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="phone-number" classname="form-label">Phone Number:</label>
-                    <TextInput id="phone-number" label="(000) 000-000" ref={phoneNum} />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="institution" classname="form-label">Institution:</label>
-                    <TextInput id="institution" label="ex: University of Maryland, College Park" ref={institution} />
-                </div>
+                <TextInput label="Institution" ref={institution} showLabel={true} placeholder="ex: University of Maryland, College Park" required={false} />
                 {/*
                 <SelectButton label="Institution" ref={institution} options={[
                     { label: 'Select Institution', value: '' },
@@ -175,9 +163,9 @@ function SignUp() {
                 ]} />
                 */}
 
-                <div className="form-group">
-                    <label htmlFor="role" classname="form-label">Role:</label>
-                    <SelectButton id="role" label="role" ref={role} options={[
+                <div className={styles.formGroup}>
+                    <label htmlFor="role" className="form-label">Role</label>
+                    <SelectButton id="role" ref={role} required={true} options={[
                         { label: 'Select Option', value: '' },
                         { label: 'High School Student', value: 'hsStudent' },
                         { label: 'Undergraduate Student', value: 'undergraduateStudent' },
@@ -188,41 +176,52 @@ function SignUp() {
                     ]} />
                 </div>
 
+                <TextInput label="Please specify if you selected 'Other'" ref={otherRole} showLabel={true} required={false} />
+
                 {userType === 'explorer' && (
                     <>
-                        <div className="form-group">
-                            <label htmlFor="major" classname="form-label">Major:</label>
-                            <TextInput id="major" label="ex: Physics" ref={major} />
-                        </div>
+                        <TextInput label="Major" ref={major} showLabel={true} placeholder="ex: Physics" required={false} />
 
-                        <div className="form-group">
+                        {/* show label does not work for drop down menus, yet */}
+                        <div className={styles.formGroup}>
                             <label htmlFor="attendance" classname="form-label">Would you be attending in person or virtually?</label>
-                            <SelectButton id="attendance" ref={attendance} options={[
+                            <SelectButton id="attendance" ref={attendance} required={true} options={[
                                 { label: 'Select Option', value: '' },
-                                { label: 'In Person', value: 'inPerson' },
+                                { label: 'In Person*', value: 'inPerson' },
                                 { label: 'Virtual', value: 'virtual' }
                             ]} />
                         </div>
+
+                        <small style={{ fontSize: '13px' }}>
+                            *Note: Due to capacity restraints, we are only accepting 100 in-person attendees. 
+                            Choosing in-person attendance does not guarantee acceptance. 
+                            We will notify you if you have been selected to attend in-person.
+                        </small>
                     </>
                 )}
 
                 <hr style={{ margin: '20px 0' }} />
 
                 {/* keep numbers as placeholders for now, will figure out later */}
-                <div className="form-group">
+                <div className={styles.formGroup}>
                     <label htmlFor="where-heard" classname="form-label">Where did you hear about this event?</label>
-                    <SelectButton id="where-heard" ref={whereHeard} options={[
+                    <SelectButton id="where-heard" ref={whereHeard} required={false} options={[
                         { label: 'Select Option', value: '' },
-                        { label: '1', value: 'one' },
-                        { label: '2', value: 'two' },
-                        { label: '3', value: 'three' },
-                        { label: '4', value: 'four' }
+                        { label: 'Instagram', value: 'instagram' },
+                        { label: 'Linkedin', value: 'linkedin' },
+                        { label: 'University/School Club', value: 'club' },
+                        { label: 'Word of mouth (e.g. friend, colleague)', value: 'wordOfMouth' },
+                        { label: 'Google Search', value: 'google' },
+                        { label: 'Flyer', value: 'flyer' },
+                        { label: 'Other', value: 'other'}
                     ]} />
                 </div>
 
+                <TextInput label="Please specify if you selected 'Other'" ref={otherWhereHeard} showLabel={true} required={false} />
+
                 {userType === 'volunteer' && (
                     <>
-                        <div className="form-group">
+                        <div className={styles.formGroup}>
                             <label classname="form-label">Where would you like to volunteer?</label>
                             <div className="checkbox-group"> {[
                                 { label: '  Registration', value: 'registration' },
@@ -243,13 +242,19 @@ function SignUp() {
                                 </div>
                             ))}
                             </div>
+                            {locationError && (
+                                /* error of not selecting any options is being tracked but not shown to user*/
+                                <p style={{ color: 'red', fontSize: '0.9rem', marginTop: '0.5rem' }}>{locationError}
+                                </p>
+                            )}
                         </div>
-                        <div className="form-group">
+
+                        <div className={styles.formGroup}>
                             <label classname="form-label">What is your availability for the event?</label>
                             <div className="checkbox-group"> {[
-                                { label: '  Friday Night', value: 'friday' },
+                                { label: '  Friday', value: 'friday' },
                                 { label: '  Saturday', value: 'saturday' },
-                                { label: '  Sunday', value: 'sunday' },
+                                { label: '  Sunday*', value: 'sunday' },
                             ].map((option) => (
                                 <div key={option.value} className="checkbox-option">
                                     <input
@@ -263,8 +268,12 @@ function SignUp() {
                                 </div>
                             ))}
                             </div>
+                            {locationError && (
+                                <p style={{ color: 'red', fontSize: '0.9rem'}}>{locationError}</p>
+                            )}
                         </div>
-                        <div className="form-group">
+
+                        <div className={styles.formGroup}>
                             <label classname="form-label">What shifts are you able to volunteer?</label>
                             <div className="checkbox-group"> {[
                                 { label: '  4:00 - 6:00 pm (Friday)', value: 'friday4to6'},
@@ -285,7 +294,16 @@ function SignUp() {
                                 </div>
                             ))}
                             </div>
+                            {locationError && (
+                                <p style={{ color: 'red', fontSize: '0.9rem'}}>{locationError}</p>
+                            )}
                         </div>
+
+                        <small style={{ fontSize: '13px' }}>
+                            *Note: Q^3 is tentatively scheduled for Sunday and may be subject to change. 
+                            We'll provide updates as soon as they are available.
+                        </small>
+
                     </>
                 )}
 
@@ -293,45 +311,45 @@ function SignUp() {
                 {userType === 'explorer' && (
                     <>
                         <br />
-                        <TextInput label="Why quantum and art?" ref={why} />
-                        <TextInput label="My favorite song is..." ref={favoriteSong} />
-                        <TextInput label="My favorite movie is..." ref={favoriteMovie} />
-                        <TextInput label="My favorite museum is..." ref={favoriteMuseum} />
+                        <TextInput label="Why quantum and art?" ref={whyQuantumArt} showLabel={true} required={false} />
+                        <br />
+
+                        <p>What's your favorite...</p>
+
+                        <TextInput label="Song" ref={favoriteSong} />
+                        <TextInput label="Movie" ref={favoriteMovie} />
+                        <TextInput label="Game" ref={favoriteGame} />
+                        <TextInput label="Book" ref={favoriteBook} />
 
                         <br />
-                        <TextInput label="A musician approaches you and asks you for a crazy idea on combining quantum and music. What would you say?" ref={artPrompt} showLabel={true} placeholder="A quantum keyboard..." />
+                        <TextInput label="If you were to write a book, what would it be about?" ref={bookPrompt} showLabel={true} 
+                        required={false} />
                         <br />
-                        <TextInput label="What if the musician said he wanted to use that idea to brainwash the world. What will you do to stop him?" ref={ethicsPrompt} showLabel={true} placeholder="You mustn't..." />
+                        <TextInput label="Suppose you were to merge quantum and music. What would you create?" ref={artPrompt} 
+                        showLabel={true} required={false} />
+                        <br />
+                        <TextInput label="When you think of quantum research, what's the first thing that comes to mind?" 
+                        ref={researchPrompt} showLabel={true} required={false} />
+                        <br />
+                        <TextInput label="What do you look forward to at Q^3?" ref={q3Prompt} showLabel={true} required={false} />
                     </>
                 )}
 
                 <hr style={{ margin: '20px 0' }} />
 
-                {/* I don't know if this exact header is correct or not */}
                 <h4>Emergency Contact Information</h4>
 
-                <div className="form-group">
-                <label htmlFor="emerg-name" classname="form-label">Name:</label>
-                    <div id="emerg-name" className={styles.formRow}>
-                        <TextInput label="First Name" ref={emergFirstName} />
-                        <TextInput label="Last Name" ref={emergLastName} />
+                <div className={styles.formGroup}>
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <div className={styles.formRow}>
+                        <TextInput id="emergFirstName" label="First Name" ref={emergFirstName} required={true} style={{ flex: 1 }} />
+                        <TextInput id="emergLastName" label="Last Name" ref={emergLastName} required={true} style={{ flex: 1 }} />
                     </div>
                 </div>
 
-                <div className="form-group">
-                    <label htmlFor="relation" classname="form-label">Relationship:</label>
-                    <TextInput id="relation" label="ex: Father" ref={emergRelation} type="email" />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="emerg-email" classname="form-label">Email:</label>
-                    <TextInput id="emerg-email" label="ex: myname@example.com" ref={emergEmail} type="email" />
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="emerg-number" classname="form-label">Phone Number:</label>
-                    <TextInput id="emerg-number" label="(000) 000-000" ref={emergNumber} />
-                </div>
+                <TextInput label="Relation" ref={emergRelation} showLabel={true} placeholder="ex: Father" required={true} />
+                <TextInput label="Email" ref={emergEmail} showLabel={true} type="email" placeholder="ex: myname@example.com" required={true} />
+                <TextInput label="Phone Number" ref={emergNumber} showLabel={true} placeholder="(000) 000-000" required={true} />
 
                 {/* Implement Terms and Conditions text and option box (box can be multiple choice) */}
 
