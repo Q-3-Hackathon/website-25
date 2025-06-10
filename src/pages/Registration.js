40d5943fa1206a217768f1754d3a0775499fb541
Diana Lin <lindiana1206@gmail.com>
2025-06-10 16:10:32 -0400

    reformat registration page

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -13,25 +13,25 @@ function Registration() {
       <div className="reg-btn">
         <div className="btn-col">
           <a id="vol-btn" href="/signup?userType=volunteer">Volunteer</a>
-          <button className="vol-sym-btn">
+          {/* <button className="vol-sym-btn">
             <img src={infoSym} alt="Volunteer info symbol icon" />
-            <div className="popup-box">
-              As a volunteer, you play a key role in ensuring the success of our initiative.
-              From managing logistics to supporting participants, your efforts help foster a smooth and welcoming experience for everyone involved!
-            </div>
-          </button>
+          </button> */}
+          <div className="popup-box">
+            As a volunteer, you play a key role in ensuring the success of our initiative.
+            From managing logistics to supporting participants, your efforts help foster a smooth and welcoming experience for everyone involved!
+          </div>
 
 
         </div>
 
         <div className="btn-col">
           <a id="explore-btn" href="/signup?userType=explorer">Explorer</a>
-          <button className="exp-sym-btn">
+          {/* <button className="exp-sym-btn">
             <img src={infoSym} alt="Explorer info symbol icon" />
-            <div className="popup-box">
-              As an explorer, you will explore the intricacies of Quantum in Art and Ethics.
-            </div>
-          </button>
+          </button> */}
+          <div className="popup-box">
+            As an explorer, you will explore the intricacies of Quantum in Art and Ethics.
+          </div>
         </div>
       </div>
     </div>
